cc2e2b24ac47fe031df9abde8c985fcb4749e3da
Andrew Downing <ad510@users.sourceforge.net>
2012-11-28 20:50:49 -0800

    various menu updates and add credits page

--- game/js/game.js
+++ game/js/game.js
@@ -8,8 +8,7 @@ make new offset monsters when adding new tile
 quadratic arrows: y = x - x^2 has y > 0 for 0 < x < 1 and vertex (0.5, 0.25)
 sound
 game balance (e.g. increase spawn rate over time)
-menu (parchment background, update instructions, submit score, view high scores)
-credit Matthew for button and parchment imgs
+menu (update instructions, submit score, view high scores)
 only allow wood cutters to turn into player?
 img title tags
 pay attention to navigation & color balance
