39dcbcc6d518e9bb907e5f1e0b5bd3f0990d9337
Andrew Downing <ad510@users.sourceforge.net>
2012-12-05 00:20:41 -0800

    always output valid html in php scripts

--- game/submithighscore.php
+++ game/submithighscore.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <!--
 Copyright (c) 2012 Andrew Downing
 
@@ -7,26 +8,36 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -->
-<p><a href="../index.htm">Main Menu</a></p>
-<p><?php
+<html>
+  <head>
+    <title>Borderless TD</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+  </head>
+  <body>
+    <p><a href="../index.htm">Main Menu</a></p>
+    <p><?php
 $con = new mysqli("localhost", "root", "5xvhK5zBC1", "borderless_td");
 if ($con->connect_errno) {
-  die("Failed to connect to MySQL: " . $con->connect_error);
+  echo("Failed to connect to MySQL: " . $con->connect_error);
 }
-if (!preg_match("/^[a-zA-Z ]+$/", $_POST["f_name"])) {
-  die("Name may only contain letters or spaces");
+else if (!preg_match("/^[a-zA-Z ]+$/", $_POST["f_name"])) {
+  echo("Name may only contain letters or spaces");
 }
 else if (!preg_match("/^[0-9]+$/", $_POST["f_score"])) {
-  die("Score may only contain digits");
+  echo("Score may only contain digits");
 }
 else if (!$con->query("insert into High_scores (name, score) values ('" . $_POST["f_name"] . "', '" . $_POST["f_score"] . "')")) {
   if (!$con->query("create table High_scores (name varchar(100), score int)")) {
-    die("Error creating high scores table: " . $con->error);
+    echo("Error creating high scores table: " . $con->error);
   }
   else if (!$con->query("insert into High_scores (name, score) values ('" . $_POST["f_name"] . "', '" . $_POST["f_score"] . "')")) {
-    die("Error adding high score: " . $con->error);
+    echo("Error adding high score: " . $con->error);
   }
 }
-header("Location: ../highscores.php");
-exit;
+else {
+  header("Location: ../highscores.php");
+  exit;
+}
 ?></p>
+  </body>
+</html>
--- highscores.php
+++ highscores.php
@@ -26,15 +26,17 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <?php
 $con = new mysqli("localhost", "root", "5xvhK5zBC1", "borderless_td");
 if ($con->connect_errno) {
-  die("Failed to connect to MySQL: " . $con->connect_error);
+  echo("</table>Failed to connect to MySQL: " . $con->connect_error);
 }
-$res = $con->query("select * from High_scores order by score desc");
-$res->data_seek(0);
-for ($i = 0; $i < 5 && $row = $res->fetch_assoc(); $i++) {
-  echo("<tr><td>" . $row["name"] . "</td><td>" . $row["score"] . "</td></tr>");
+else {
+  $res = $con->query("select * from High_scores order by score desc");
+  $res->data_seek(0);
+  for ($i = 0; $i < 5 && $row = $res->fetch_assoc(); $i++) {
+    echo("<tr><td>" . $row["name"] . "</td><td>" . $row["score"] . "</td></tr>");
+  }
+  echo("</table>");
 }
         ?>
-        </table>
       </div>
       <a href="index.htm"><div class="button" style="background-image: url('img/button0.png')">Main Menu</div></a>
     </div>
