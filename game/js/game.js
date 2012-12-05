c6708913c304e9b407d85cd1c21c39a7d3010e6c
Andrew Downing <ad510@users.sourceforge.net>
2012-12-05 00:12:43 -0800

    add server-side input validation

--- game/js/game.js
+++ game/js/game.js
@@ -13,7 +13,6 @@ make new offset monsters when adding new tile
 quadratic arrows: y = x - x^2 has y > 0 for 0 < x < 1 and vertex (0.5, 0.25)
 sound
 tell user when not enough wood to build?
-server-side input validation
 pay attention to navigation & color balance
 check project requirements
 */
--- game/submithighscore.php
+++ game/submithighscore.php
@@ -13,9 +13,13 @@ $con = new mysqli("localhost", "root", "5xvhK5zBC1", "borderless_td");
 if ($con->connect_errno) {
   die("Failed to connect to MySQL: " . $con->connect_error);
 }
-// unfortunately there is currently no input validation, which allows for SQL injection
-// if I had more time for the project I would add that
-if (!$con->query("insert into High_scores (name, score) values ('" . $_POST["f_name"] . "', '" . $_POST["f_score"] . "')")) {
+if (!preg_match("/^[a-zA-Z ]+$/", $_POST["f_name"])) {
+  die("Name may only contain letters or spaces");
+}
+else if (!preg_match("/^[0-9]+$/", $_POST["f_score"])) {
+  die("Score may only contain digits");
+}
+else if (!$con->query("insert into High_scores (name, score) values ('" . $_POST["f_name"] . "', '" . $_POST["f_score"] . "')")) {
   if (!$con->query("create table High_scores (name varchar(100), score int)")) {
     die("Error creating high scores table: " . $con->error);
   }
